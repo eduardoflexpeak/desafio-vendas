dcc36bab864b67815ddd379652473302d84728b1
Eduardo <eduardo@dudstecnologia.com.br>
2020-02-02 12:29:49 -0400

    Auth e AdminLTE

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Bem Vindo ao Sistema de Vendas</p>
+@stop
+
+@section('css')
+@stop
+
+@section('js')
+@stop
+
